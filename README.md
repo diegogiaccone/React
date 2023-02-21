e7f79ec176b1b0324ab9121f41e0e676942eae7c
diegoG <diego_fg91@hotmail.com>
2023-02-21 11:45:13 -0300

    agregue README con el GIFT

---
+++ README.md
@@ -0,0 +1,7 @@
+# Funko Pops
+
+Se trata de una tienda e-commerce de venta de muñecos funko de colección como proyecto para Coderhouse
+
+## Gift
+
+[Gift de navegabilidad](https://files.fm/u/hyq44qa65)
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Cart = () => {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();    
+  };
   return (
-    <div>Cart</div>
+    <div className='forma'>
+    <form className='formulario' onSubmit={handleSubmit}>
+      <input className='controles' type="text" onChange={(e) => setNombre(e.target.value)} placeholder="Nombre" required/>
+      <input className='controles' type="text" onChange={(e) => setApellido(e.target.value)} placeholder="Apellido" required/>
+      <input className='controles' type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email" required/>
+      <textarea className='textarea' placeholder="Comentario" required cols="30" rows="10"/>
+      <button className='controles' type='submit'>Enviar Informacion</button>
+    </form>
+    </div>
   )
 }
 
